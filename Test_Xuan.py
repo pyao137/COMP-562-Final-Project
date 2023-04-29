b7f5fd34961301fbd09ec18c1e70f5aa085bd98b
xuanbai01 <xuanbai@ad.unc.edu>
2023-04-28 21:03:01 -0400

    cleaning up BodyType

--- Test_Xuan.py
+++ Test_Xuan.py
@@ -3,28 +3,27 @@ import pandas as pd
 import csv
 from sklearn.utils import Bunch
 
-
-#Clean up zipcode
+# Clean up zipcode
 df = pd.read_csv('Datasets/Ebay Used Cars.csv')
 ndf = df[df['zipcode'].str.len() == 5]
 ndf = ndf[ndf['zipcode'].str[2:5] != '***']
 ndf['zipcode'] = ndf['zipcode'].str[:-2]
-df['zipcode'] = pd.to_numeric(ndf['zipcode'], downcast = 'integer').astype('Int64')
+df['zipcode'] = pd.to_numeric(ndf['zipcode'], downcast='integer').astype('Int64')
 df = df.dropna()
-df['zipcode'] = df['zipcode'].astype('int64')  
+df['zipcode'] = df['zipcode'].astype('int64')
 
-#Drop unneeded columns
+# Drop unneeded columns
 df = df.drop('Trim', axis=1)
 df = df.drop('Engine', axis=1)
 
-#Clean up drive type and num-cylinders
+# Clean up drive type and num-cylinders
 df = df[df['DriveType'].str.len() == 3]
 df = df[df['NumCylinders'] != 0]
 print(len(df))
 
-print (df.dtypes)
+print(df.dtypes)
 
-#Find the top 20 variables in BodyType
+# Find the top 20 variables in BodyType
 df['BodyType'] = df['BodyType'].str.lower()
 top_20 = df['BodyType'].value_counts().head(20)
 print(top_20)
@@ -32,9 +31,10 @@ sum_counts = top_20.sum()
 # Print the sum of the value counts
 print(sum_counts)
 
-#Clean up BodyType
+# Clean up BodyType
 df = df[df['BodyType'].isin(top_20.index)]
 print(len(df))
 
-
-
+# Added a new column for the the age of the car
+df['CarAge'] = 2023 - df['Year']
+df.info()
