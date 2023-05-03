d57b07fdbb4bff15bd98f53fba9576c68d3b965d
Peter Yao <ypeter2212@gmail.com>
2023-05-03 17:34:12 -0400

    more work done

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # COMP-562-Final-Project
 
-Dataset: https://www.kaggle.com/datasets/thedevastator/uncovering-factors-that-affect-used-car-prices
+Dataset: https://www.kaggle.com/datasets/tsaustin/us-used-car-sales-data 
 
 test 2
--- Test_Peter.py
+++ Test_Peter.py
@@ -3,27 +3,42 @@ import pandas as pd
 import csv
 from sklearn.utils import Bunch
 
+#Read the file
 df = pd.read_csv('Datasets/Ebay Used Cars.csv')
 
 # Drop unneeded columns
 df = df.drop('Trim', axis=1)
 df = df.drop('Engine', axis=1)
+df = df.drop('yearsold', axis = 1)
+df = df.drop('NumCylinders', axis = 1)
 
-# Clean up drive type and num-cylinders
+# Clean up drive type and num-cylinders (drop 0 cylinder cars and drop outlier DriveTypes)
 df = df[df['DriveType'].str.len() == 3]
 df['DriveType'] = df['DriveType'].str.lower()
-top_drive_types = df['DriveType'].value_counts().head(7)
-df = df[df['DriveType'].isin(top_drive_types.index)]
-df = df[df['NumCylinders'] != 0]
+drive_types = df['DriveType'].value_counts().gt(10)
+df = df.loc[df['DriveType'].isin(drive_types[drive_types].index)]
+#df = df[df['NumCylinders'] != 0]
 
-# Find the top 20 variables in BodyType
+#Clean up BodyType (Drop outliers with freq. under 50)
 df['BodyType'] = df['BodyType'].str.lower()
-print(df['BodyType'].value_counts())
-top_20 = df['BodyType'].value_counts().head(20)
-sum_counts = top_20.sum()
+body_types = df['BodyType'].value_counts().gt(50)
+df = df.loc[df['BodyType'].isin(body_types[body_types].index)]
 
-# Clean up BodyType
-df = df[df['BodyType'].isin(top_20.index)]
+#Clean up model (Drop outliers with freq. under 20)
+df['Model'] = df['Model'].str.lower()
+models = df['Model'].value_counts().gt(20)
+df = df.loc[df['Model'].isin(models[models].index)]
+
+#Clean up Make (Drop outliers with freq. under 20)
+df['Make'] = df['Make'].str.lower()
+makes = df['Make'].value_counts().gt(20)
+df = df.loc[df['Make'].isin(makes[makes].index)]
+
+#Clean up numerical values
+df = df[df['Mileage'] > 100]
+df = df[df['Mileage'] < 400000]
+df = df[df['Year'] > 1920]
+df = df[df['pricesold'] > 200]
 
 # Clean up zipcode and drop nulls
 ndf = df[df['zipcode'].str.len() == 5]
@@ -40,9 +55,9 @@ df.info()
 
 #TODO
 # Drop rows with mileage and price under a certain extreme amount
-# Drop years which are not in a certain range (say 1950 - 2020). Drop yearsold not in reasonable range.
-# Drop rows where Make and Model have values which are extremely low frequency (under 10 occurrences)
+# Drop years which are not in a certain range (say 1920 - 2020). Drop yearsold column
+# Drop rows where Make and Model have values which are extremely low frequency (under 20 occurrences)
 # Instead of only keeping the first x most common frequencies of BodyType and DriveType, drop those entries
   # whose frequencies are outlier-range (say under 10 occurrences)
   # Possibly drop BodyType column entirely as this may be dependent variable on make and model
-  
+# How to handle electric cars? Perhaps drop the num-cylinders column entirely?
