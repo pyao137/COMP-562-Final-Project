6d83bb77ab0d9aac02fbc65df5cef07fa9765e76
xuanbai01 <xuanbai@ad.unc.edu>
2023-05-07 23:09:11 -0400

    Graphs

--- Test_Xuan.py
+++ Test_Xuan.py
@@ -1,135 +1,220 @@
-import numpy as np
+import math
 import pandas as pd
-import csv
-from sklearn.utils import Bunch
-from sklearn.experimental import enable_hist_gradient_boosting
+import numpy as np
 from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error ,mean_absolute_error
+from sklearn.inspection import permutation_importance
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
 from scipy.stats import skew
 
-plt.style.use('ggplot')
-sns.set()
+
+DROP_CYL = False #DON'T CHANGE
+
+ONE_HOT = False
+
+def one_hot(df, feature):
+    dummies = pd.get_dummies(df[[feature]])
+    res = pd.concat([df, dummies], axis=1)
+    res = res.drop([feature], axis=1)
+    return(res)
+
+def ordinal(df, feature):
+  df[feature] = pd.factorize(df[feature])[0] + 1
+
+def get_scores(model_name, model, val_x): #inputted model must be trained
+  y_predictions = model.predict(val_x)
+  mae = mean_absolute_error(val_y, y_predictions)
+  print(f"{model_name} absolute mean error: {mae}")
+  mse = mean_squared_error(val_y, y_predictions)
+  rmse = math.sqrt(mse)
+  print(f"{model_name} root mean squared error: {rmse}")
+
+def get_scores_ensembled(model_name, model_list, val_x): #inputted model must be trained. We assume there are 5 models in model list
+  pred_list = []
+  y_predictions = []
+  for i in range(5): #There will be 5 models in the model_list
+    pred_list.append(model_list[i].predict(val_x))
+  for i in range(len(pred_list[0])):
+    sum = 0
+    for j in range(5):
+      sum += pred_list[j][i]
+    y_predictions.append(sum/5)
+  mae = mean_absolute_error(val_y, y_predictions)
+  print(f"{model_name} absolute mean error: {mae}")
+  mse = mean_squared_error(val_y, y_predictions)
+  rmse = math.sqrt(mse)
+  print(f"{model_name} root mean squared error: {rmse}")
+
+def get_feature_importance(model_name, model, val_x, val_y):
+  r = permutation_importance(model, val_x, val_y, n_repeats=10, random_state=0)
+  print(f'Feature importances for {model_name}')
+  for i in range(len(r.importances_mean)):
+    print(f"  {df.columns[i + 1]}: {r.importances_mean[i]}")
 
 
-def one_hot(original_dataframe, feature_to_encode):
-    dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
-    res = pd.concat([original_dataframe, dummies], axis=1)
-    res = res.drop([feature_to_encode], axis=1)
-    return (res)
-
-
-# Read the file
+#Read the file
 df = pd.read_csv('Datasets/Ebay Used Cars.csv')
+etest = df[df['Engine'].str.lower() == 'electric'] #2 no non-tesla makes have more than 15 sales as marked electric cars:
+#print(etset['Model'].value_counts().gt(10))
 
 # Drop unneeded columns
 df = df.drop('Trim', axis=1)
 df = df.drop('Engine', axis=1)
-df = df.drop('yearsold', axis=1)
-df = df.drop('NumCylinders', axis=1)
-df = df.drop('ID', axis=1)
+df = df.drop('yearsold', axis = 1)
+df = df.drop('ID', axis = 1)
 
-# Clean up drive type and num-cylinders (drop 0 cylinders cars and drop outlier DriveTypes)
+#Possible: Drop num cylinders
+if (DROP_CYL):
+  df = df.drop('NumCylinders', axis = 1)
+
+# Clean up drive type (drop outlier DriveTypes)
 df = df[df['DriveType'].str.len() == 3]
 df['DriveType'] = df['DriveType'].str.lower()
 drive_types = df['DriveType'].value_counts().gt(10)
 df = df.loc[df['DriveType'].isin(drive_types[drive_types].index)]
-# df = df[df['NumCylinders'] != 0]
 
-# Clean up BodyType (Drop outliers with freq. under 50)
+#Clean up BodyType (Drop outliers with freq. under 50)
 df['BodyType'] = df['BodyType'].str.lower()
 body_types = df['BodyType'].value_counts().gt(50)
 df = df.loc[df['BodyType'].isin(body_types[body_types].index)]
 
-# Clean up model (Drop outliers with freq. under 20)
+#Clean up model (Drop outliers with freq. under 20)
 df['Model'] = df['Model'].str.lower()
 models = df['Model'].value_counts().gt(20)
 df = df.loc[df['Model'].isin(models[models].index)]
 
-# Clean up Make (Drop outliers with freq. under 20)
+#Clean up Make (Drop outliers with freq. under 20)
 df['Make'] = df['Make'].str.lower()
 makes = df['Make'].value_counts().gt(20)
 df = df.loc[df['Make'].isin(makes[makes].index)]
 
-# Clean up numerical values
+#Clean up numerical values
 df = df[df['Mileage'] > 100]
 df = df[df['Mileage'] < 400000]
 df = df[df['Year'] > 1920]
+df = df[df['Year'] < 2021]
 df = df[df['pricesold'] > 200]
+
+#Clean up num-cylinders if not dropped
+if (DROP_CYL == False):
+  df = df[ (df['NumCylinders'] != 0) | (df['Make'] == 'tesla')]
 
 # Clean up zipcode and drop nulls
 ndf = df[df['zipcode'].str.len() == 5]
 ndf = ndf[ndf['zipcode'].str[2:5] != '***']
 ndf['zipcode'] = ndf['zipcode'].str[:-2]
 df['zipcode'] = pd.to_numeric(ndf['zipcode'], downcast='integer').astype('Int64')
-df = df.dropna()  # Drop nulls
+df = df.dropna() #Drop nulls
 df['zipcode'] = df['zipcode'].astype('int64')
 
-# Added a new column for the age of the car
-df['CarAge'] = 2023 - df['Year']
+# # Years over Prices
+# var = 'Year'
+# data = pd.concat([df['pricesold'], df[var]], axis=1)
+# f, ax = plt.subplots(figsize=(20, 10))
+# fig = sns.boxplot(x=var, y="pricesold", data=data)
+# fig.axis(ymin=0, ymax=240000);
+# plt.xticks(rotation=90);
+# plt.show()
+#
+# # Numbers of count of Make
+# var = "Make"
+# plt.figure(figsize=(20, 10))
+# sns.catplot(x=var, kind="count", palette="ch:.25", height=8, aspect=2, data=df);
+# plt.xticks(rotation=90);
+# plt.show()
+#
 
-# #One-hot encode all categorical variables
-# one_hot_features = ['Make', 'Model', 'BodyType', 'DriveType']
-# for feature in one_hot_features:
+# Milage
+
+
+
+# #Encode categorical variables
+# cat_feat = ['Make', 'Model', 'BodyType', 'DriveType']
+# if (ONE_HOT):
+#   for feature in cat_feat:
 #     df = one_hot(df, feature)
+# else:
+#   for feature in cat_feat:
+#     ordinal(df, feature)
 #
-#
-# #Randomize rows and split dataset into train, test, and validation data
+# #Randomize rows and split dataset into train and validation data
 # df = df.sample(frac=1)
-# df.info()
 #
-# train_df = df.iloc[:55000,:]
-# #test_df = df.iloc[50000:59000,:]
-# val_df = df.iloc[55000:,:]
+# if ('NumCylinders' in df):
+#   train_df = df.iloc[:53000,:]
+#   val_df = df.iloc[53000:,:]
+# else:
+#   train_df = df.iloc[:58000,:]
+#   val_df = df.iloc[58000:,:]
 #
-# #Train the model
-# df_x = df.drop("pricesold", axis = 1).to_numpy()
-# df_y = (df["pricesold"]).to_numpy()
 # train_x = train_df.drop("pricesold", axis = 1).to_numpy()
 # train_y = (train_df["pricesold"]).to_numpy()
 # val_x = val_df.drop("pricesold", axis = 1).to_numpy()
 # val_y = (val_df["pricesold"]).to_numpy()
-# regressor = HistGradientBoostingRegressor()
-# regressor.fit(df_x, df_y)
-# print(regressor.score(df_x, df_y))
+#
+# #Split train data into 5-folds:
+# train_sets_x = []
+# train_sets_y = []
+# for i in range(5):
+#   new_df = df.drop(df.index[i*10500:(i+1)*10500], inplace = False)
+#   train_sets_y.append((new_df["pricesold"]).to_numpy())
+#   train_sets_x.append(new_df.drop("pricesold", axis = 1).to_numpy())
+# print(len(train_sets_x[0]))
+# print(df.info())
+#
+#
+# #Train hreg
+# hreg = HistGradientBoostingRegressor(
+#     max_iter = 1000,
+#     loss = 'squared_error'
+# )
+# hreg.fit(train_x ,train_y)
+# get_scores("hreg", hreg, val_x)
+# get_feature_importance('hreg', hreg, val_x, val_y)
+#
+# #Train ensembled hreg
+# ensemble_hreg = []
+# for i in range(len(train_sets_x)):
+#   hreg = HistGradientBoostingRegressor(
+#     max_iter = 1000,
+#     loss = 'squared_error'
+#   )
+#   hreg.fit(train_sets_x[i], train_sets_y[i])
+#   ensemble_hreg.append(hreg)
+# get_scores_ensembled("ens-hreg", ensemble_hreg, val_x)
 
-# f, ax = plt.subplots(figsize=(15, 8))
-# sns.displot(df['pricesold'])
-# plt.xlim([0, 160000])
 
-plt.style.use('ggplot')
-# select all quantitative columns for checking the spread
-numeric_columns = df.select_dtypes(include=np.number).columns.tolist()
-plt.figure(figsize=(20, 25))
+# #Train models
+# for i in range(5):
+#   print(f"trial {i}")
+#   RFreg = RandomForestRegressor().fit(train_x, train_y)
+#   print(RFreg.score(val_x, val_y))
+#
+#   hreg = HistGradientBoostingRegressor().fit(train_x, train_y)
+#   print(hreg.score(val_x, val_y))
+#
+#   gbreg = GradientBoostingRegressor().fit(train_x, train_y)
+#   print(gbreg.score(val_x, val_y))
+#
+#   ridgereg = Ridge().fit(train_x, train_y)
+#   print(ridgereg.score(val_x, val_y))
+#
+#   linreg = LinearRegression().fit(df_x, df_y)
+#   print(linreg.score(df_x, df_y))
 
-for i, variable in enumerate(numeric_columns):
-    plt.subplot(10, 3, i + 1)
+# #Cross val
+# from sklearn.model_selection import cross_val_score
+# hgreg = HistGradientBoostingRegressor(loss = 'absolute_error').fit(train_x, train_y)
+# print(cross_val_score(hreg, val_x, val_y, cv=5, scoring='neg_mean_absolute_error'))
 
-    sns.distplot(df[variable], kde=False, color='blue')
-    plt.tight_layout()
-    plt.title(variable)
-plt.show()
 
-cat_columns = ['Make', 'Mileage', 'DriveType', 'pricesold']  # cars.select_dtypes(
-# exclude=np.number).columns.tolist()
-
-plt.figure(figsize=(15, 21))
-
-for i, variable in enumerate(cat_columns):
-    plt.subplot(4, 2, i + 1)
-    order = df[variable].value_counts(ascending=False).index
-    ax = sns.countplot(x=df[variable], data=df, order=order, palette='viridis')
-    for p in ax.patches:
-        percentage = '{:.1f}%'.format(100 * p.get_height() / len(df[variable]))
-        x = p.get_x() + p.get_width() / 2 - 0.05
-        y = p.get_y() + p.get_height()
-        plt.annotate(percentage, (x, y), ha='center')
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.title(variable)
-
-plt.show()
 # TODO
 # Drop rows with mileage and price under a certain extreme amount
 # Drop years which are not in a certain range (say 1920 - 2020). Drop yearsold column
