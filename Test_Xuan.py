70059a6d218078e21ad394f18428fec08d90a18f
xuanbai01 <xuanbai@ad.unc.edu>
2023-05-07 23:37:50 -0400

    Graphs done

--- Test_Xuan.py
+++ Test_Xuan.py
@@ -15,6 +15,7 @@ import scipy.stats as stats
 from scipy.stats import skew
 
 
+
 DROP_CYL = False #DON'T CHANGE
 
 ONE_HOT = False
@@ -114,25 +115,34 @@ df['zipcode'] = pd.to_numeric(ndf['zipcode'], downcast='integer').astype('Int64'
 df = df.dropna() #Drop nulls
 df['zipcode'] = df['zipcode'].astype('int64')
 
-# # Years over Prices
-# var = 'Year'
-# data = pd.concat([df['pricesold'], df[var]], axis=1)
-# f, ax = plt.subplots(figsize=(20, 10))
-# fig = sns.boxplot(x=var, y="pricesold", data=data)
-# fig.axis(ymin=0, ymax=240000);
-# plt.xticks(rotation=90);
-# plt.show()
-#
-# # Numbers of count of Make
-# var = "Make"
-# plt.figure(figsize=(20, 10))
-# sns.catplot(x=var, kind="count", palette="ch:.25", height=8, aspect=2, data=df);
-# plt.xticks(rotation=90);
-# plt.show()
-#
+# Years vs Price Sold
+var = 'Year'
+data = pd.concat([df['pricesold'], df[var]], axis=1)
+f, ax = plt.subplots(figsize=(20, 10))
+fig = sns.boxplot(x=var, y="pricesold", data=data)
+fig.axis(ymin=0, ymax=240000);
+plt.xticks(rotation=90);
+plt.show()
 
-# Milage
+# Numbers of count of Make
+var = "Make"
+plt.figure(figsize=(20, 10))
+sns.catplot(x=var, kind="count", palette="ch:.25", height=8, aspect=2, data=df);
+plt.xticks(rotation=90);
+plt.show()
 
+
+# Mileage vs Price Sold
+plt.figure(figsize=(20, 8))
+hb = plt.hexbin(df['Mileage'], df['pricesold'], gridsize=(100, 50), cmap='viridis', mincnt=1, vmax=250)
+plt.xlabel('Mileage')
+plt.ylabel('Price Sold')
+plt.title('Mileage vs Price Sold')
+plt.xlim(0, 420000)
+plt.ylim(0, 240000)
+cb = plt.colorbar(hb, label='Number of Cars')
+cb.set_ticks([0, 50, 100, 150, 200, 250])
+plt.show()
 
 
 # #Encode categorical variables
